91d5ede0eacdb8768d87ffb56b648e60d64f574e
Alison Carrera <alison.carrera@corp.globo.com>
2019-06-13 14:48:12 -0300

    Adjusts in dependencies.

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='onn',
-      version='0.0.1',
+      version='0.0.3',
       description='Online Neural Network',
       url='https://github.com/alison-carrera/onn',
       author='Alison Carrera',
       author_email='alison.carrera2007@gmail.com',
       packages=find_packages(),
-      install_requires=['numpy', 'pytorch'],
+      install_requires=['numpy', 'torch'],
       long_description=long_description,
       long_description_content_type='text/markdown',
       license='Apache 2.0',
